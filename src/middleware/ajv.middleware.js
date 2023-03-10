689c16e9a990bc72ad64a4329dd9c6534a5cb220
unknown <amarjeet@baruzotech.com>
2023-03-10 10:18:24 +0530

    add ejs file

---
+++ src/middleware/ajv.middleware.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function validateSchema(ajv) {
+    return (req, res, next) => {
+        const valid = ajv(req.body);
+        if (!valid) {
+            const error = ajv.errors;
+            if (error[0].params.allowedValues) {
+                return res.status(400).send(`${error[0].message} ${error[0].params.allowedValues}`);
+            }
+            return res.status(400).send(error[0].message);
+        }
+        next();
+    };
+}
+exports.default = validateSchema;
