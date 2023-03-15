5c228c399ad7d20aeaa231eee8c17f7e5ecea956
unknown <amarjeet@baruzotech.com>
2023-03-15 10:16:10 +0530

    add color
    add npm color only effective in console.log

--- dist/server.js
+++ dist/server.js
@@ -7,9 +7,11 @@ const express_1 = __importDefault(require("express"));
 const dotenv_1 = require("dotenv");
 (0, dotenv_1.config)();
 const morgan_1 = __importDefault(require("morgan"));
+const colors_1 = __importDefault(require("colors")); //import colors from npm
 const config_js_1 = require("./config.js");
 const index_js_1 = __importDefault(require("./routes/index.js"));
 const mongoose_service_js_1 = require("./services/mongoose.service.js");
+colors_1.default.enable();
 (0, mongoose_service_js_1.connectDatabase)();
 const app = (0, express_1.default)();
 app.use((0, morgan_1.default)('dev'));
@@ -20,5 +22,5 @@ app.get('/', (req, res) => {
 });
 app.use('/', index_js_1.default);
 app.listen(config_js_1.port, () => {
-    console.log(`app is running on PORT ${config_js_1.port}`);
+    console.log(`app is running on PORT ${config_js_1.port}`.yellow);
 });
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express, { Request, Response } from 'express'
-import {config} from 'dotenv'
+import { config } from 'dotenv'
 config()
 import morgan from 'morgan'
+import colors from 'colors' //import colors from npm
 import { port } from './config.js';
 import router from './routes/index.js';
 import { connectDatabase } from './services/mongoose.service.js';
-
+colors.enable();
 connectDatabase()
 
 const app = express()
@@ -20,6 +21,6 @@ app.use('/', router)
 
 
 app.listen(port, () => {
-    console.log(`app is running on PORT ${port}`);
+    console.log(`app is running on PORT ${port}`.yellow);
 })
 
